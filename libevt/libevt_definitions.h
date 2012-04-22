4d727ee34eab241f8a8eb667c000d0227bf71c49
Joachim Metz <joachim.metz@gmail.com>
2012-04-22 20:00:06 +0200

    Changes of version: 20120422

--- libevt/libevt_definitions.h
+++ libevt/libevt_definitions.h
@@ -37,11 +37,11 @@
  * for local use of libevt
  */
 #else
-#define LIBEVT_VERSION						20120413
+#define LIBEVT_VERSION						20120422
 
 /* The version string
  */
-#define LIBEVT_VERSION_STRING					"20120413"
+#define LIBEVT_VERSION_STRING					"20120422"
 
 /* The access flags definitions
  * bit 1        set to 1 for read access
