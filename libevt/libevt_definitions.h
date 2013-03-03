20a8ce49b7f573799e72b1398e117300c4e1c3d4
Joachim Metz <joachim.metz@gmail.com>
2013-03-03 18:30:37 +0100

    Changes of version: 20130303

--- libevt/libevt_definitions.h
+++ libevt/libevt_definitions.h
@@ -37,11 +37,11 @@
  * for local use of libevt
  */
 #else
-#define LIBEVT_VERSION						20130226
+#define LIBEVT_VERSION						20130303
 
 /* The version string
  */
-#define LIBEVT_VERSION_STRING					"20130226"
+#define LIBEVT_VERSION_STRING					"20130303"
 
 /* The access flags definitions
  * bit 1        set to 1 for read access
