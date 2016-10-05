52cf352a3713a19689a9124e364cac373968e561
Joachim Metz <joachim.metz@gmail.com>
2016-10-05 07:23:05 +0200

    Worked on tests.

--- tests/evt_test_file.c
+++ tests/evt_test_file.c
@@ -27,15 +27,421 @@
 #endif
 
 #include "evt_test_libcerror.h"
+#include "evt_test_libclocale.h"
 #include "evt_test_libcstring.h"
 #include "evt_test_libcsystem.h"
 #include "evt_test_libevt.h"
+#include "evt_test_libuna.h"
 #include "evt_test_macros.h"
 #include "evt_test_memory.h"
+
+#if SIZEOF_WCHAR_T != 2 && SIZEOF_WCHAR_T != 4
+#error Unsupported size of wchar_t
+#endif
 
 /* Define to make evt_test_file generate verbose output
 #define EVT_TEST_FILE_VERBOSE
  */
+
+/* Retrieves source as a narrow string
+ * Returns 1 if successful or -1 on error
+ */
+int evt_test_file_get_narrow_source(
+     const libcstring_system_character_t *source,
+     char *narrow_string,
+     size_t narrow_string_size,
+     libcerror_error_t **error )
+{
+	static char *function     = "evt_test_file_get_narrow_source";
+	size_t narrow_source_size = 0;
+	size_t source_length      = 0;
+
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
+	int result                = 0;
+#endif
+
+	if( source == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid source.",
+		 function );
+
+		return( -1 );
+	}
+	if( narrow_string == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid narrow string.",
+		 function );
+
+		return( -1 );
+	}
+	if( narrow_string_size > (size_t) SSIZE_MAX )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_VALUE_EXCEEDS_MAXIMUM,
+		 "%s: invalid narrow string size value exceeds maximum.",
+		 function );
+
+		return( -1 );
+	}
+	source_length = libcstring_system_string_length(
+	                 source );
+
+	if( source_length > (size_t) ( SSIZE_MAX - 1 ) )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_VALUE_OUT_OF_BOUNDS,
+		 "%s: invalid source length value out of bounds.",
+		 function );
+
+		return( -1 );
+	}
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
+	if( libclocale_codepage == 0 )
+	{
+#if SIZEOF_WCHAR_T == 4
+		result = libuna_utf8_string_size_from_utf32(
+		          (libuna_utf32_character_t *) source,
+		          source_length + 1,
+		          &narrow_source_size,
+		          error );
+#elif SIZEOF_WCHAR_T == 2
+		result = libuna_utf8_string_size_from_utf16(
+		          (libuna_utf16_character_t *) source,
+		          source_length + 1,
+		          &narrow_source_size,
+		          error );
+#endif
+	}
+	else
+	{
+#if SIZEOF_WCHAR_T == 4
+		result = libuna_byte_stream_size_from_utf32(
+		          (libuna_utf32_character_t *) source,
+		          source_length + 1,
+		          libclocale_codepage,
+		          &narrow_source_size,
+		          error );
+#elif SIZEOF_WCHAR_T == 2
+		result = libuna_byte_stream_size_from_utf16(
+		          (libuna_utf16_character_t *) source,
+		          source_length + 1,
+		          libclocale_codepage,
+		          &narrow_source_size,
+		          error );
+#endif
+	}
+	if( result != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_CONVERSION,
+		 LIBCERROR_CONVERSION_ERROR_GENERIC,
+		 "%s: unable to determine narrow string size.",
+		 function );
+
+		return( -1 );
+	}
+#else
+	narrow_source_size = source_length + 1;
+
+#endif /* defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER ) */
+
+	if( narrow_string_size < narrow_source_size )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_VALUE_TOO_SMALL,
+		 "%s: narrow string too small.",
+		 function );
+
+		return( -1 );
+	}
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
+	if( libclocale_codepage == 0 )
+	{
+#if SIZEOF_WCHAR_T == 4
+		result = libuna_utf8_string_copy_from_utf32(
+		          (libuna_utf8_character_t *) narrow_string,
+		          narrow_string_size,
+		          (libuna_utf32_character_t *) source,
+		          source_length + 1,
+		          error );
+#elif SIZEOF_WCHAR_T == 2
+		result = libuna_utf8_string_copy_from_utf16(
+		          (libuna_utf8_character_t *) narrow_string,
+		          narrow_string_size,
+		          (libuna_utf16_character_t *) source,
+		          source_length + 1,
+		          error );
+#endif
+	}
+	else
+	{
+#if SIZEOF_WCHAR_T == 4
+		result = libuna_byte_stream_copy_from_utf32(
+		          (uint8_t *) narrow_string,
+		          narrow_string_size,
+		          libclocale_codepage,
+		          (libuna_utf32_character_t *) source,
+		          source_length + 1,
+		          error );
+#elif SIZEOF_WCHAR_T == 2
+		result = libuna_byte_stream_copy_from_utf16(
+		          (uint8_t *) narrow_string,
+		          narrow_string_size,
+		          libclocale_codepage,
+		          (libuna_utf16_character_t *) source,
+		          source_length + 1,
+		          error );
+#endif
+	}
+	if( result != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_CONVERSION,
+		 LIBCERROR_CONVERSION_ERROR_GENERIC,
+		 "%s: unable to set narrow string.",
+		 function );
+
+		return( -1 );
+	}
+#else
+	if( libcstring_system_string_copy(
+	     narrow_string,
+	     source,
+	     source_length ) == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_MEMORY,
+		 LIBCERROR_MEMORY_ERROR_COPY_FAILED,
+		 "%s: unable to set narrow string.",
+		 function );
+
+		return( -1 );
+	}
+	narrow_string[ source_length ] = 0;
+
+#endif /* defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER ) */
+
+	return( 1 );
+}
+
+#if defined( HAVE_WIDE_CHARACTER_TYPE )
+
+/* Retrieves source as a wide string
+ * Returns 1 if successful or -1 on error
+ */
+int evt_test_file_get_wide_source(
+     const libcstring_system_character_t *source,
+     wchar_t *wide_string,
+     size_t wide_string_size,
+     libcerror_error_t **error )
+{
+	static char *function   = "evt_test_file_get_wide_source";
+	size_t wide_source_size = 0;
+	size_t source_length    = 0;
+
+#if !defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
+	int result              = 0;
+#endif
+
+	if( source == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid source.",
+		 function );
+
+		return( -1 );
+	}
+	if( wide_string == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid wide string.",
+		 function );
+
+		return( -1 );
+	}
+	if( wide_string_size > (size_t) SSIZE_MAX )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_VALUE_EXCEEDS_MAXIMUM,
+		 "%s: invalid wide string size value exceeds maximum.",
+		 function );
+
+		return( -1 );
+	}
+	source_length = libcstring_system_string_length(
+	                 source );
+
+	if( source_length > (size_t) ( SSIZE_MAX - 1 ) )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_VALUE_OUT_OF_BOUNDS,
+		 "%s: invalid source length value out of bounds.",
+		 function );
+
+		return( -1 );
+	}
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
+	wide_source_size = source_length + 1;
+#else
+	if( libclocale_codepage == 0 )
+	{
+#if SIZEOF_WCHAR_T == 4
+		result = libuna_utf8_string_size_from_utf32(
+		          (libuna_utf32_character_t *) source,
+		          source_length + 1,
+		          &wide_source_size,
+		          error );
+#elif SIZEOF_WCHAR_T == 2
+		result = libuna_utf8_string_size_from_utf16(
+		          (libuna_utf16_character_t *) source,
+		          source_length + 1,
+		          &wide_source_size,
+		          error );
+#endif
+	}
+	else
+	{
+#if SIZEOF_WCHAR_T == 4
+		result = libuna_byte_stream_size_from_utf32(
+		          (libuna_utf32_character_t *) source,
+		          source_length + 1,
+		          libclocale_codepage,
+		          &wide_source_size,
+		          error );
+#elif SIZEOF_WCHAR_T == 2
+		result = libuna_byte_stream_size_from_utf16(
+		          (libuna_utf16_character_t *) source,
+		          source_length + 1,
+		          libclocale_codepage,
+		          &wide_source_size,
+		          error );
+#endif
+	}
+	if( result != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_CONVERSION,
+		 LIBCERROR_CONVERSION_ERROR_GENERIC,
+		 "%s: unable to determine wide string size.",
+		 function );
+
+		return( -1 );
+	}
+
+#endif /* defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER ) */
+
+	if( wide_string_size < wide_source_size )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_VALUE_TOO_SMALL,
+		 "%s: wide string too small.",
+		 function );
+
+		return( -1 );
+	}
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
+	if( libcstring_system_string_copy(
+	     wide_string,
+	     source,
+	     source_length ) == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_MEMORY,
+		 LIBCERROR_MEMORY_ERROR_COPY_FAILED,
+		 "%s: unable to set wide string.",
+		 function );
+
+		return( -1 );
+	}
+	wide_string[ source_length ] = 0;
+#else
+	if( libclocale_codepage == 0 )
+	{
+#if SIZEOF_WCHAR_T == 4
+		result = libuna_utf8_string_copy_from_utf32(
+		          (libuna_utf8_character_t *) wide_string,
+		          wide_string_size,
+		          (libuna_utf32_character_t *) source,
+		          source_length + 1,
+		          error );
+#elif SIZEOF_WCHAR_T == 2
+		result = libuna_utf8_string_copy_from_utf16(
+		          (libuna_utf8_character_t *) wide_string,
+		          wide_string_size,
+		          (libuna_utf16_character_t *) source,
+		          source_length + 1,
+		          error );
+#endif
+	}
+	else
+	{
+#if SIZEOF_WCHAR_T == 4
+		result = libuna_byte_stream_copy_from_utf32(
+		          (uint8_t *) wide_string,
+		          wide_string_size,
+		          libclocale_codepage,
+		          (libuna_utf32_character_t *) source,
+		          source_length + 1,
+		          error );
+#elif SIZEOF_WCHAR_T == 2
+		result = libuna_byte_stream_copy_from_utf16(
+		          (uint8_t *) wide_string,
+		          wide_string_size,
+		          libclocale_codepage,
+		          (libuna_utf16_character_t *) source,
+		          source_length + 1,
+		          error );
+#endif
+	}
+	if( result != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_CONVERSION,
+		 LIBCERROR_CONVERSION_ERROR_GENERIC,
+		 "%s: unable to set wide string.",
+		 function );
+
+		return( -1 );
+	}
+
+#endif /* defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER ) */
+
+	return( 1 );
+}
+
+#endif /* defined( HAVE_WIDE_CHARACTER_TYPE ) */
 
 /* Creates and opens a source file
  * Returns 1 if successful or -1 on error
@@ -374,6 +780,234 @@ on_error:
 	}
 	return( 0 );
 }
+
+/* Tests the libevt_file_open functions
+ * Returns 1 if successful or 0 if not
+ */
+int evt_test_file_open(
+     const libcstring_system_character_t *source )
+{
+	char narrow_source[ 128 ];
+
+	libcerror_error_t *error = NULL;
+	libevt_file_t *file      = NULL;
+	int result               = 0;
+
+	/* Initialize test
+	 */
+	result = evt_test_file_get_narrow_source(
+	          source,
+	          narrow_source,
+	          128,
+	          &error );
+
+	EVT_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+        EVT_TEST_ASSERT_IS_NULL(
+         "error",
+         error );
+
+	result = libevt_file_initialize(
+	          &file,
+	          &error );
+
+	EVT_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+        EVT_TEST_ASSERT_IS_NOT_NULL(
+         "file",
+         file );
+
+        EVT_TEST_ASSERT_IS_NULL(
+         "error",
+         error );
+
+	/* Test open
+	 */
+	result = libevt_file_open(
+	          file,
+	          narrow_source,
+	          LIBEVT_OPEN_READ,
+	          &error );
+
+	EVT_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+        EVT_TEST_ASSERT_IS_NULL(
+         "error",
+         error );
+
+	/* Clean up
+	 */
+	result = libevt_file_close(
+	          file,
+	          &error );
+
+	EVT_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
+        EVT_TEST_ASSERT_IS_NULL(
+         "error",
+         error );
+
+	result = libevt_file_free(
+	          &file,
+	          &error );
+
+	EVT_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+        EVT_TEST_ASSERT_IS_NULL(
+         "file",
+         file );
+
+        EVT_TEST_ASSERT_IS_NULL(
+         "error",
+         error );
+
+	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	if( file != NULL )
+	{
+		libevt_file_free(
+		 &file,
+		 NULL );
+	}
+	return( 0 );
+}
+
+#if defined( HAVE_WIDE_CHARACTER_TYPE )
+
+/* Tests the libevt_file_open_wide functions
+ * Returns 1 if successful or 0 if not
+ */
+int evt_test_file_open_wide(
+     const libcstring_system_character_t *source )
+{
+	wchar_t wide_source[ 128 ];
+
+	libcerror_error_t *error = NULL;
+	libevt_file_t *file      = NULL;
+	int result               = 0;
+
+	/* Initialize test
+	 */
+	result = evt_test_file_get_wide_source(
+	          source,
+	          wide_source,
+	          128,
+	          &error );
+
+	EVT_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+        EVT_TEST_ASSERT_IS_NULL(
+         "error",
+         error );
+
+	result = libevt_file_initialize(
+	          &file,
+	          &error );
+
+	EVT_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+        EVT_TEST_ASSERT_IS_NOT_NULL(
+         "file",
+         file );
+
+        EVT_TEST_ASSERT_IS_NULL(
+         "error",
+         error );
+
+	/* Test open
+	 */
+	result = libevt_file_open_wide(
+	          file,
+	          wide_source,
+	          LIBEVT_OPEN_READ,
+	          &error );
+
+	EVT_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+        EVT_TEST_ASSERT_IS_NULL(
+         "error",
+         error );
+
+	/* Clean up
+	 */
+	result = libevt_file_close(
+	          file,
+	          &error );
+
+	EVT_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
+        EVT_TEST_ASSERT_IS_NULL(
+         "error",
+         error );
+
+	result = libevt_file_free(
+	          &file,
+	          &error );
+
+	EVT_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+        EVT_TEST_ASSERT_IS_NULL(
+         "file",
+         file );
+
+        EVT_TEST_ASSERT_IS_NULL(
+         "error",
+         error );
+
+	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	if( file != NULL )
+	{
+		libevt_file_free(
+		 &file,
+		 NULL );
+	}
+	return( 0 );
+}
+
+#endif /* defined( HAVE_WIDE_CHARACTER_TYPE ) */
 
 /* Tests the libevt_file_get_ascii_codepage functions
  * Returns 1 if successful or 0 if not
@@ -741,10 +1375,18 @@ int main(
 #if !defined( __BORLANDC__ ) || ( __BORLANDC__ >= 0x0560 )
 	if( source != NULL )
 	{
-		/* TODO add test for libevt_file_open */
+		EVT_TEST_RUN_WITH_ARGS(
+		 "libevt_file_open",
+		 evt_test_file_open,
+		 source );
 
 #if defined( HAVE_WIDE_CHARACTER_TYPE )
-		/* TODO add test for libevt_file_open_wide */
+
+		EVT_TEST_RUN_WITH_ARGS(
+		 "libevt_file_open_wide",
+		 evt_test_file_open_wide,
+		 source );
+
 #endif /* defined( HAVE_WIDE_CHARACTER_TYPE ) */
 
 		/* TODO add test for libevt_file_open_file_io_handle */
