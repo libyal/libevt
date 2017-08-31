5606275eb8a92234738a0ba3b5693a21e448ef4c
Joachim Metz <joachim.metz@gmail.com>
2017-08-31 08:27:47 +0200

    Worked on tests

--- tests/evt_test_record.c
+++ tests/evt_test_record.c
@@ -55,9 +55,9 @@ int evt_test_record_free(
 	 result,
 	 -1 );
 
-        EVT_TEST_ASSERT_IS_NOT_NULL(
-         "error",
-         error );
+	EVT_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
 
 	libcerror_error_free(
 	 &error );
