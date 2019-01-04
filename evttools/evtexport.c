e5b5649849c1f5de7866d878086e3e4d05ce2e29
Joachim Metz <joachim.metz@gmail.com>
2019-01-04 15:53:54 +0100

    Applied updates

--- evttools/evtexport.c
+++ evttools/evtexport.c
@@ -1,7 +1,7 @@
 /*
  * Extracts items from a Windows Event Log (EVT) file
  *
- * Copyright (C) 2011-2018, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2019, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- libevt/evt_file_header.h
+++ libevt/evt_file_header.h
@@ -1,7 +1,7 @@
 /*
  * The file header definition of a Windows Event Log (EVT)
  *
- * Copyright (C) 2011-2018, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2019, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
