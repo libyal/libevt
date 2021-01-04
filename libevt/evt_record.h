da06d90e44f718bd7cc9e1ef91fe19cf0bf48cb6
Joachim Metz <joachim.metz@gmail.com>
2021-01-04 09:13:03 +0100

    Applied updates

--- libevt/evt_record.h
+++ libevt/evt_record.h
@@ -1,7 +1,7 @@
 /*
  * The item definition of a Windows Event Log (EVT)
  *
- * Copyright (C) 2011-2020, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2021, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
