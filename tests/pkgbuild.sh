0cdc3ba93777ebec35ed0be3bca793f6c35349d3
Joachim Metz <joachim.metz@gmail.com>
2026-09-15 05:30:09 +0200

    Worked on tests and changes for CI tests

--- tests/pkgbuild.sh
+++ tests/pkgbuild.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # Script to build a MacOS pkg
 #
-# Version: 20241015
+# Version: 20260914
 
 set -e
 
-make install DESTDIR=${PWD}/osx-pkg
-mkdir -p ${PWD}/osx-pkg/usr/share/doc/libevt
-cp AUTHORS COPYING COPYING.LESSER NEWS README ${PWD}/osx-pkg/usr/share/doc/libevt
+make install DESTDIR="${PWD}/osx-pkg"
+mkdir -p "${PWD}/osx-pkg/usr/share/doc/libevt"
+cp AUTHORS COPYING COPYING.LESSER NEWS README "${PWD}/osx-pkg/usr/share/doc/libevt"
 
-VERSION=`sed '5!d; s/^  \[//;s/\],$//' configure.ac`
-pkgbuild --root osx-pkg --identifier com.github.libyal.libevt --version ${VERSION} --ownership recommended ../libevt-${VERSION}.pkg
+VERSION=$(sed '5!d; s/^  \[//;s/\],$//' configure.ac)
+pkgbuild --root osx-pkg --identifier com.github.libyal.libevt --version "${VERSION}" --ownership recommended "../libevt-${VERSION}.pkg"
 
